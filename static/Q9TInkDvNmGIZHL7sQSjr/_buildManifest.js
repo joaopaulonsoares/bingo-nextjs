8068d6dfd30eeaa51bc31918b0d608711e7f2c75
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2021-06-14 17:19:29 -0300

    Adding static version of site

---
+++ static/Q9TInkDvNmGIZHL7sQSjr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/da8511982b0b5a05fa29.css","static/chunks/pages/index-630916c615ea4b467750.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
